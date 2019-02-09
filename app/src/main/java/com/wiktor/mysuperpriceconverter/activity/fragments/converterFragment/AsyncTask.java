b14ad2bfae2894947d337efba75376e50e7ec35c
Wiktor22 <wiktor@bk.ru>
2019-02-09 12:28:58 +0300

    3

---
+++ app/src/main/java/com/wiktor/mysuperpriceconverter/activity/fragments/converterFragment/AsyncTask.java
@@ -0,0 +1,18 @@
+package com.wiktor.mysuperpriceconverter.activity.fragments.converterFragment;
+
+public class AsyncTask  extends android.os.AsyncTask<Void, Void, Void> {
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
